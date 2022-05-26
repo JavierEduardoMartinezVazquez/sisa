f04a4281ba14347e5f16b91150505a5c9fd915e0
Javier Eduardo <al222010046@gmail.com>
2022-05-26 09:52:38 -0500

    20052022

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ $app = new Illuminate\Foundation\Application(
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------
 |
 | Next, we need to bind some important interfaces into the container so
 | we will be able to resolve them when needed. The kernels serve the
