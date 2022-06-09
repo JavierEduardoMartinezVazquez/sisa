2d3067de8ca354f41b4559d7c2efba05791bb37e
Javier Eduardo <al222010046@gmail.com>
2022-06-09 12:38:37 -0500

    09062022sisaCORTE

---
+++ app/Asisstances.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Assistances extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        /*'name', 'email', 'password',*/
+        'name',
+        /*'lastname_p',
+        'lastname_m',*/
+        'email',
+        'email_verified_at',
+        'password',
+        /*'fechaingresocorp',
+        'fechaingresoemp',
+        'fechabaja',
+        'id_horario',
+        'id_empresa',
+        'id_area',
+        'id_rol',
+        'status',*/
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
--- resources/views/control/secciones/menu.blade.php
+++ resources/views/control/secciones/menu.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: rgb(55, 55, 55)">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: rgb(85, 85, 85)">
     <div class="sidebar">
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
